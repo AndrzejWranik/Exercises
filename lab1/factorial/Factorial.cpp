3535ac5501cfafa8a89b14d560aaae33cfd20dbb
Andrzej <wranikandrzej@gmail.com>
2017-03-14 14:01:47 +0100

    Implement factorial function

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,8 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if ((value > 0) && (value < 13)) return value * factorial(value - 1);
+  if ((value < 0) && (value > -12)) return value * factorial(value + 1);
+  if (value == 0) return 1;
+  if ((value >= 13) || (value <= -12)) return 0;
 }
